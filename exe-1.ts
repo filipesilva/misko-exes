071db180d0b41bd4880716d10cb9c5fd7a3445e1
Filipe Silva <filipematossilva@gmail.com>
2018-02-10 12:32:47 +0000

    add exe-1 sum
    12:22 to 12:28
    
    While copying over, I noticed that I forgot `this.` in the val1 and val2 acessors.

--- exe-1.ts
+++ exe-1.ts
@@ -8,12 +8,20 @@ interface Node {
 }
 
 class Value implements Node {
-  constructor(private val) { }
+  constructor(private val: number) { }
   eval = () => this.val;
   toString = () => this.val.toString();
 }
 
+class Sum implements Node {
+  constructor(private val1: Node, private val2: Node) { }
+  eval = () => this.val1.eval() + this.val2.eval();
+  toString = () => `${this.val1.toString()}+${this.val2.toString()}`;
+}
 
 // Tests
 expect(new Value(5).eval(), 5);
 expect(new Value(5).toString(), '5');
+expect(new Sum(new Value(1), new Value(2)).eval(), 3);
+expect(new Sum(new Value(1), new Sum(new Value(2), new Value(3))).eval(), 6);
+expect(new Sum(new Value(1), new Sum(new Value(2), new Value(3))).toString(), '1+2+3');
