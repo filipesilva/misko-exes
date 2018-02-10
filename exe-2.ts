f245bd2fa42cd9dda5ab40156c35041cc9acabfd
Filipe Silva <filipematossilva@gmail.com>
2018-02-10 14:20:41 +0000

    add exe-2 calcLine
    13:39 to 13:52
    
    In this exercise there were some implementation bits missing so I started with those.
    
    To figure out how `calcLine` should work I put down the line equation (`y=mx+b`), wrote the function header, then started applying equation transformations to simplify it into something I could code.
    
    Using a set of points I could easily picture in my head (1,0 and 2,1) and set about making the calculations that would give me m=1 and b=-1.
    
    First I assumed `b=0` and turned the base equation into `m=(p2.y -p1.y)/(p2.x - p2.x)`.
    
    Having `m`, I resolved for b using one of the known points, thus `b = p1.y - m*p1.x`.
    
    I didn't particularly care about the numerical values, but rather wanted a single value with which to reference this line.
    
    So I settled on a target precision (5), rounded `m` using and `b`, and used them on a string of the format `y=mx+b`. I didn't remember the JS function to round to decimals and assumed it was `Math.round()`. While implementing, it turns out to be `.toFixed()`. I think the interviewer would have just told me that.
    
    This way I could use the string for my hash map directly, and return it directly at the end.
    
    While running tests I saw that the divisions could return Infinity. I tried to figure out how to address by drawing the points for which that would happen, and saw the line would be vertical.
    
    The `y=mx+b` formula cannot deal with vertical lines because they never cross the `y` axis. This is a problem because two different vertical lines, crossing the `x` axis at different points, are not the same line.
    
    I thought I could get around this by 'inverting' the axis and also calculating `x=my+b`, appending this to the string. I didn't do it because I thought it was mostly a discussion point that the interviewer would bring up, and talking through a solution would be good enough.

--- exe-2.ts
+++ exe-2.ts
@@ -12,8 +12,8 @@ function lineCrossingMostPoints(points: Point[]) {
     for (let j = i; j < points.length; j++) {
       const point1 = points[i];
       const point2 = points[j];
+      const currLine = calcLine(point1, point2);
       // Does not compile.
-      // const currLine = calcLine(point1, point2);
       // const currCount = (map.get(currLine) || 0) + 1;
       // map.put(currLine, currCount);
       // if (currCount > maxCount) {
@@ -26,5 +26,16 @@ function lineCrossingMostPoints(points: Point[]) {
 }
 
 
+function calcLine(p1: Point, p2: Point) {
+  // Using the y=mx+b equation.
+  const m = (p2.y - p1.y) / (p2.x - p1.x);
+  const b = p1.y - m * p1.x;
+  const precision = 5;
+  return `y=${m.toFixed(precision)}x+${b.toFixed(precision)}`;
+}
+
+
 // Tests
 expect(lineCrossingMostPoints([]), undefined);
+expect(calcLine(new Point(1, 0), new Point(2, 1)), 'y=1.00000x+-1.00000');
+expect(calcLine(new Point(2, 0), new Point(1, 0.5)), 'y=-0.50000x+1.00000');
