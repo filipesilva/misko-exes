5afbcced8a68d5df949327d844ce997b65720efb
Filipe Silva <filipematossilva@gmail.com>
2018-02-10 14:28:38 +0000

    add exe-2 map
    14:21 to 14:25
    
    Having the string representation for the line, adding a hash map was trivial. Strings are easy to compare even though they are not the same object.
    
    I added a test for a number of points of which I knew what the result would be and verified it.

--- exe-2.ts
+++ exe-2.ts
@@ -7,19 +7,19 @@ class Point {
 }
 
 function lineCrossingMostPoints(points: Point[]) {
-  let maxLine, maxCount;
+  let maxLine, maxCount = 0;
+  const map = new Map<string, number>();
   for (let i = 0; i < points.length - 1; i++) {
     for (let j = i; j < points.length; j++) {
       const point1 = points[i];
       const point2 = points[j];
       const currLine = calcLine(point1, point2);
-      // Does not compile.
-      // const currCount = (map.get(currLine) || 0) + 1;
-      // map.put(currLine, currCount);
-      // if (currCount > maxCount) {
-      //   maxLine = currLine;
-      //   maxCount = currCount;
-      // }
+      const currCount = (map.get(currLine) || 0) + 1;
+      map.set(currLine, currCount);
+      if (currCount > maxCount) {
+        maxLine = currLine;
+        maxCount = currCount;
+      }
     }
   }
   return maxLine;
@@ -36,6 +36,11 @@ function calcLine(p1: Point, p2: Point) {
 
 
 // Tests
-expect(lineCrossingMostPoints([]), undefined);
 expect(calcLine(new Point(1, 0), new Point(2, 1)), 'y=1.00000x+-1.00000');
 expect(calcLine(new Point(2, 0), new Point(1, 0.5)), 'y=-0.50000x+1.00000');
+expect(lineCrossingMostPoints([
+  new Point(1, 0),
+  new Point(2, 1),
+  new Point(3, 2),
+  new Point(2, 0),
+]), 'y=1.00000x+-1.00000');
