bec51258981acb7c39d3537a1f5e158d27a263fa
Filipe Silva <filipematossilva@gmail.com>
2018-02-10 12:19:03 +0000

    add given information

--- exe-1.ts
+++ exe-1.ts
@@ -1,3 +1,19 @@
 import { expect } from './expect';
 
-console.log('hello')
+
+// Implementation
+interface Node {
+  eval(): number;
+  toString(): string;
+}
+
+class Value implements Node {
+  constructor(private val) { }
+  eval = () => this.val;
+  toString = () => this.val.toString();
+}
+
+
+// Tests
+expect(new Value(5).eval(), 5);
+expect(new Value(5).toString(), '5');
--- exe-2.ts
+++ exe-2.ts
@@ -1,3 +1,30 @@
 import { expect } from './expect';
 
-console.log('world')
+
+// Implementation
+class Point {
+  constructor(public x: number, public y: number) { }
+}
+
+function lineCrossingMostPoints(points: Point[]) {
+  let maxLine, maxCount;
+  for (let i = 0; i < points.length - 1; i++) {
+    for (let j = i; j < points.length; j++) {
+      const point1 = points[i];
+      const point2 = points[j];
+      // Does not compile.
+      // const currLine = calcLine(point1, point2);
+      // const currCount = (map.get(currLine) || 0) + 1;
+      // map.put(currLine, currCount);
+      // if (currCount > maxCount) {
+      //   maxLine = currLine;
+      //   maxCount = currCount;
+      // }
+    }
+  }
+  return maxLine;
+}
+
+
+// Tests
+expect(lineCrossingMostPoints([]), undefined);
