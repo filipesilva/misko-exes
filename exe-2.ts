2f111926985fd6ff4ca8024535f129d726010c44
Filipe Silva <filipematossilva@gmail.com>
2018-02-19 21:18:42 +0000

    add exe-2 serializeLine
    Separate `calcLine` into calculation and serialization, fix https://github.com/filipesilva/misko-exes/commit/f245bd2fa42cd9dda5ab40156c35041cc9acabfd#r27539366.

--- exe-2.ts
+++ exe-2.ts
@@ -6,6 +6,10 @@ class Point {
   constructor(public x: number, public y: number) { }
 }
 
+class Line {
+  constructor(public m: number, public b: number) { }
+}
+
 function lineCrossingMostPoints(points: Point[]) {
   let maxLine, maxCount = 0;
   const map = new Map<string, number>();
@@ -13,11 +17,12 @@ function lineCrossingMostPoints(points: Point[]) {
     for (let j = i; j < points.length; j++) {
       const point1 = points[i];
       const point2 = points[j];
-      const currLine = calcLine(point1, point2);
-      const currCount = (map.get(currLine) || 0) + 1;
-      map.set(currLine, currCount);
+      const lineInstance = calcLine(point1, point2);
+      const serializedLine = serializeLine(lineInstance);
+      const currCount = (map.get(serializedLine) || 0) + 1;
+      map.set(serializedLine, currCount);
       if (currCount > maxCount) {
-        maxLine = currLine;
+        maxLine = serializedLine;
         maxCount = currCount;
       }
     }
@@ -30,14 +35,18 @@ function calcLine(p1: Point, p2: Point) {
   // Using the y=mx+b equation.
   const m = (p2.y - p1.y) / (p2.x - p1.x);
   const b = p1.y - m * p1.x;
-  const precision = 5;
-  return `y=${m.toFixed(precision)}x+${b.toFixed(precision)}`;
+  return new Line(m, b);
 }
 
 
+function serializeLine(line: Line){
+  const precision = 5;
+  return `y=${line.m.toFixed(precision)}x+${line.b.toFixed(precision)}`;
+}
+
 // Tests
-expect(calcLine(new Point(1, 0), new Point(2, 1)), 'y=1.00000x+-1.00000');
-expect(calcLine(new Point(2, 0), new Point(1, 0.5)), 'y=-0.50000x+1.00000');
+expect(serializeLine(calcLine(new Point(1, 0), new Point(2, 1))), 'y=1.00000x+-1.00000');
+expect(serializeLine(calcLine(new Point(2, 0), new Point(1, 0.5))), 'y=-0.50000x+1.00000');
 expect(lineCrossingMostPoints([
   new Point(1, 0),
   new Point(2, 1),
